1095fc400e923ab71b366fd8c86219450fb70b0f
rui jing <jingrui5@163.com>
2023-09-15 14:53:09 +0100

    add a PersonalContactCategory class

---
+++ src/main/java/PersonalContactCategory.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PersonalContactCategory implements ContactCategory{
+
+    public void addContact(){};
+    public void removeContact(){};
+    public void viewContacts(){};
+    public void searchContacts(){};
+
+    HashMap<String,Contact> personalContactMap = new HashMap<>();
+
+
+
+
+
+
+}
